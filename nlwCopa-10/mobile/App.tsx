4242ab8bab1c33a73d3f430c99149c8756a1d270
Guilherme Alves Araujo <guilherme-allves@hotmail.com>
2022-11-02 16:11:48 -0300

    nlwCopa - primeira parte back, web e mobile finalizada

--- nlwCopa-10/mobile/App.tsx
+++ nlwCopa-10/mobile/App.tsx
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider, Box } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular,  Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
+
+import { THEME } from './styles/theme';
 
 
 export default function App() {
+  const [fontsLoad] = useFonts({ Roboto_400Regular,  Roboto_500Medium, Roboto_700Bold });  
+
+
   return (
-    <NativeBaseProvider>
-      <Box flex={1} bg="gray" alignItems="center" justifyContent="center">Hello world!!</Box>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {
+        fontsLoad ? <SignIn /> : <Loading />
+      }      
     </NativeBaseProvider>
   );
 }
