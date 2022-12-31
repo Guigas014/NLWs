b89d4ccc21a3a1d933d87614bde577f0f1e697e7
Guilherme Alves Araujo <guilherme-allves@hotmail.com>
2022-12-31 01:04:06 -0300

    nlwCopa - Estilização do Ranking finalizada

--- nlwCopa-10/mobile/src/components/Ranking.tsx
+++ nlwCopa-10/mobile/src/components/Ranking.tsx
@@ -7,6 +7,9 @@ import { EmptyRakingList } from '../components/EmptyRakingList';
 
 
 export function Ranking() {
+  const position = 4;
+
+
   return (
     //<FlatList
       ////data={}
@@ -25,29 +28,54 @@ export function Ranking() {
       mb={3}
       p={4}
     >
+      <HStack 
+        w="full"
+        mx={2}
+        py={2}
+        justifyContent="space-between"
+        alignItems="center" 
+      >
+
       <HStack>
-        <Avatar
-          //key={participant.id}
-          source={{ uri: "https://github.com/Guigas014.png"}}
-          w={8}
-          h={8}
-          rounded="full"
-          borderWidth={2}
-          marginRight={-3}
-          borderColor="gray.800"
-       />
+          <Avatar
+            //key={participant.id}
+            source={{ uri: "https://github.com/Guigas014.png"}}
+            w={12}
+            h={12}
+            rounded="full"
+            borderWidth={2}
+            marginRight={-3}
+            borderColor="gray.800"
+          />
 
-       <VStack>
-         <Heading color="white" fontSize="md" fontFamily="heading">
-           Guilherme
-         </Heading>
+          <VStack ml={5} justifyContent="center">
+            <Heading color="white" fontSize="md" fontFamily="heading" mb={0.5}>
+              Guilherme Alves
+              <Text color="gray.200" fontSize="xs">(você)</Text>
+            </Heading>
 
-         <Text color="gray.200" fontSize="xs">
-           30 ponto(s)
-         </Text>
-       </VStack>
+            <Text color="gray.200" fontSize="xs">
+              30 ponto(s)
+            </Text>
+          </VStack>
+        </HStack>
 
-       <Text color="white" fontSize="sm">1°</Text>    
+       <Box 
+        bgColor={position > 3 ? "gray.600" : "yellow.500"} 
+        w={10} 
+        h={6} 
+        rounded="full" 
+        alignItems="center"
+        justifyContent="center" 
+      >
+         <Text 
+          color={position > 3 ? "gray.300" : "black"} 
+          fontSize="md" 
+          fontFamily="medium"
+         >
+          1°
+         </Text>    
+       </Box>
 
      </HStack>
 
