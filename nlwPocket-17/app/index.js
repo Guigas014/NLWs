be21289382b0faecc217dfc300c49249b1200560
Guilherme <guilherme-allves@hotmail.com>
2024-09-11 17:12:39 -0300

    NLW Pocket 17 - trilha Iniciante menu criado usando  inquirer

--- nlwPocket-17/app/index.js
+++ nlwPocket-17/app/index.js
@@ -1,1 +1,44 @@
-console.log("TESTE!!")
+const { select } = require("@inquirer/prompts")
+
+const start = async () => {
+  while (true) {
+    let opcao = await select({
+      message: "Menu >",
+      choices: [
+        {
+          name: "Cadastrar meta",
+          value: "cadastrar",
+        },
+        {
+          name: "Listar metas realizadas",
+          value: "listar realizadas",
+        },
+        {
+          name: "Listar metas abertas",
+          value: "listar abertas",
+        },
+        {
+          name: "Sair",
+          value: "sair",
+        },
+      ],
+    })
+
+    switch (opcao) {
+      case "cadastrar":
+        console.log("Vamos cadastrar!")
+        break
+      case "listar":
+        console.log("Vamos listar!")
+        break
+      case "sair":
+        console.log("Até a próxima!!")
+        return
+
+      default:
+        break
+    }
+  }
+}
+
+start()
