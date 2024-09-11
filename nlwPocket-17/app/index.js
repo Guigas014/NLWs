71ff87fbc775c80cfa2f46d2f17de7a2f0fbf848
Guilherme <guilherme-allves@hotmail.com>
2024-09-11 18:16:26 -0300

    NLW Pocket 17 - trilha Iniciante listar feito

--- nlwPocket-17/app/index.js
+++ nlwPocket-17/app/index.js
@@ -1,4 +1,4 @@
-const { select, input } = require("@inquirer/prompts")
+const { select, input, checkbox } = require("@inquirer/prompts")
 
 let metas = []
 
@@ -11,6 +11,35 @@ async function cadastrarMeta() {
   }
 
   metas.push({ value: meta, checked: false })
+}
+
+async function listarMetas() {
+  const respostas = await checkbox({
+    message:
+      "Use as setas para mudar de meta, o espaço para marcar e desmarcar e o Enter para finalizar essa etapa",
+    choices: [...metas],
+    instructions: false,
+  })
+
+  if (respostas.length == 0) {
+    console.log("Nenhuma meta selecionada!")
+    return
+  }
+
+  //Desmarca todas as metas
+  metas.forEach((m) => {
+    m.checked = false
+  })
+
+  respostas.forEach((resposta) => {
+    const meta = metas.find((m) => {
+      return m.value == resposta
+    })
+
+    meta.checked = true
+  })
+
+  console.log("Meta(s) marcada(s) como concluída(s)!")
 }
 
 const start = async () => {
@@ -28,7 +57,7 @@ const start = async () => {
         },
         {
           name: "Listar metas abertas",
-          value: "listar abertas",
+          value: "listar",
         },
         {
           name: "Sair",
@@ -40,10 +69,10 @@ const start = async () => {
     switch (opcao) {
       case "cadastrar":
         await cadastrarMeta()
-        console.log(metas)
+        //   console.log(metas)
         break
       case "listar":
-        console.log("Vamos listar!")
+        await listarMetas()
         break
       case "sair":
         console.log("Até a próxima!!")
