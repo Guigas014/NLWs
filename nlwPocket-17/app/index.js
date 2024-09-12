2bfb030b10ebf7aba4d413a6421caddcc1d45f5a
Guilherme <guilherme-allves@hotmail.com>
2024-09-11 23:36:48 -0300

    NLW Pocket 17 - trilha Iniciante deletar metas feito

--- nlwPocket-17/app/index.js
+++ nlwPocket-17/app/index.js
@@ -74,6 +74,31 @@ async function metasAbertas() {
   })
 }
 
+async function deletaMetas() {
+  const metasDesmarcadas = metas.map((meta) => {
+    return { value: meta.value, checked: false }
+  })
+
+  const itemsParaDeletar = await checkbox({
+    message: "Selecione uma meta para deletar",
+    choices: [...metasDesmarcadas],
+    instructions: false,
+  })
+
+  if (itemsParaDeletar.length == 0) {
+    console.log("Nenhum item para deletar!")
+    return
+  }
+
+  itemsParaDeletar.forEach((item) => {
+    metas = metas.filter((meta) => {
+      return meta.value != item
+    })
+  })
+
+  console.log("Meta(s) deleta(s) com sucesso!!")
+}
+
 const start = async () => {
   while (true) {
     const opcao = await select({
@@ -96,6 +121,10 @@ const start = async () => {
           value: "abertas",
         },
         {
+          name: "Deletar metas",
+          value: "deletar",
+        },
+        {
           name: "Sair",
           value: "sair",
         },
@@ -115,6 +144,9 @@ const start = async () => {
         break
       case "abertas":
         await metasAbertas()
+        break
+      case "deletar":
+        await deletaMetas()
         break
       case "sair":
         console.log("Até a próxima!!")
