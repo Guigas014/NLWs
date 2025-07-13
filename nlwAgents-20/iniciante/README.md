5e454e2b0361ccc1fc8a62ec224066e7668e2c91
Guilherme <guilherme-allves@hotmail.com>
2025-07-13 18:36:45 -0300

    NLW Agents 20 - trilha Iniciante / terceira aula / finalizada

---
+++ nlwAgents-20/iniciante/README.md
@@ -0,0 +1,11 @@
+# Google Gemini
+
+Site que tem as referências da API do Gemini e gera "Chaves API".
+
+**https://ai.google.dev/gemini-api/docs?hl=pt-br#rest**
+
+<br>
+
+# Showdown
+
+Transforma MARKDOWN em HTML. Pois a IA retorna os textos, por padrão, no formato Markdown.
--- nlwAgents-20/iniciante/script.js
+++ nlwAgents-20/iniciante/script.js
@@ -5,8 +5,119 @@ const questionInput = document.getElementById("questionInput")
 const askButton = document.getElementById("askButton")
 const aiResponse = document.getElementById("aiResponse")
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault()
 
-  console.log(event.target[0].value)
+  const apiKey = apiKeyInput.value
+  const game = gameSelect.value
+  const question = questionInput.value
+
+  // console.log({ apiKey, game, question })
+
+  if (apiKey == "" || game == "" || question == "") {
+    alert("Por favor, preencha todos os campos.")
+
+    return
+  }
+
+  // Muda o estado do botão quando está perguntando
+  askButton.disabled = true
+  askButton.textContent = "Perguntando..."
+  askButton.classList.add("loading")
+
+  try {
+    // Perguntar para IA
+    const text = await perguntarAI(apiKey, game, question)
+
+    aiResponse.querySelector(".response-content").innerHTML =
+      markdownToHtml(text)
+
+    aiResponse.classList.remove("hidden")
+  } catch (error) {
+    console.log("Erro: ", error)
+  } finally {
+    askButton.disabled = false
+    askButton.textContent = "Perguntar"
+    askButton.classList.remove("loading")
+  }
 })
+
+const perguntarAI = async (apiKey, game, question) => {
+  const model = "gemini-2.5-flash"
+
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+
+  // Prompt Enginer
+  // olha, tenho esse jogo ${game} e gostaria de saber ${question} // modelo antiga e básico
+  const pergunta = `
+    ## Especialidade
+    Você é um especialista assistente de meta para o jogo ${game}
+
+    ## Tarefa
+    Você deve responder as perguntas do usuário com base no seu conhecimento sobre o jogo, estratégias, build e dicas
+
+    ## Regras
+    - Se você não souber a resposta, responda com 'Não sei' e não tente inventar uma resposta.
+    - Se a pergunta não for sobre o jogo, responda com 'Essa pergunta não é sobre o jogo ${game}'.
+    - Considere a data atual ${new Date().toLocaleDateString("pt-BR")}
+    - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coerente.
+    - Nunca responda itens que você não tenha certeza de que existe no patch atual.
+
+    ## Resposta
+    - Economize na resposta, seja direto e responda no máximo 500 caracateres.
+    - Responda em Markdown.
+    - Não precisa fazer nenhuma saudação ou despedida, apenas responda a pergunta.
+
+    ## Exemplo de resposta
+    pergunta do usuário: "Qual é a melhor build para o campeão X no patch atual?"
+    resposta> "A melhor build para o campeão X no patch atual é: 
+    \n\n **Itens:** 
+    \n\n coloque aqui os itens recomendados
+    \n\n **Runas:**
+    \n\n coloque aqui as runas recomendadas
+
+    ---
+    Aqui está a pergunta do usuário: ${question}
+  `
+
+  const contents = [
+    {
+      role: "user",
+      parts: [
+        {
+          text: pergunta,
+        },
+      ],
+    },
+  ]
+
+  // Ferramentas que a IA pode usar
+  // Aqui estamos usando a ferramenta de busca do Google
+  const tools = [
+    {
+      google_search: {},
+    },
+  ]
+
+  // chamada para API
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      contents,
+      tools,
+    }),
+  })
+
+  const data = await response.json()
+  // console.log({ data })
+
+  return data.candidates[0].content.parts[0].text
+}
+
+const markdownToHtml = (text) => {
+  const converter = new showdown.Converter()
+  return converter.makeHtml(text)
+}
